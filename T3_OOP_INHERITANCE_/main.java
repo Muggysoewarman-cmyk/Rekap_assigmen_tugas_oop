049b15229cfe3055a13450b939f3dd55714591b0
Muggysoewarman-cmyk <muggysoewarman3@gmail.com>
2025-10-29 22:47:43 +0700

    Tugas ke 3

---
+++ T3_OOP_INHERITANCE_/main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package T3_OOP_INHERITANCE_;
+
+/**
+ *
+ * @author Chandra Damanik
+ */
+public class main {
+    
+}
